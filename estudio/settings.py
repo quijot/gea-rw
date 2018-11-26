e9e1e020fd9cb8083941616cd0172e1d2fcb7f73
quijoT <santiagonob@gmail.com>
2018-11-26 17:37:50 -0300

    Added django_heroku
    Signed-off-by: quijoT <santiagonob@gmail.com>

--- estudio/settings.py
+++ estudio/settings.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,6 +107,7 @@ USE_TZ = True
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
