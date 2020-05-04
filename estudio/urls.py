d2f94a3351c6b66fdb9ba232b69d348780f25075
Santiago Pestarini <santiagonob@gmail.com>
2020-05-04 14:26:03 -0300

    Fixed DEBUG var problem
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- estudio/urls.py
+++ estudio/urls.py
@@ -8,24 +8,12 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^admin/filebrowser/', include(site.urls)),
-    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
-    url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^gea/', include('gea.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^$', Home.as_view(), name='home'),
-    url(r'^nested_admin/', include('nested_admin.urls')),
+    url(r"^admin/filebrowser/", include(site.urls)),
+    url(r"^grappelli/", include("grappelli.urls")),  # grappelli URLS
+    url(r"^accounts/login/$", auth_views.login, name="login"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^gea/", include("gea.urls")),
+    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
+    url(r"^$", Home.as_view(), name="home"),
+    url(r"^nested_admin/", include("nested_admin.urls")),
 ]
-
-
-from django.conf import settings
-
-
-if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns = urlpatterns + [
-        url(r'^(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT}),
-    ]
-
