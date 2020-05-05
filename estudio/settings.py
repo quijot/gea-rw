690dfc94b25b1e70518e72bf190cc5f0ceb30138
Santiago Pestarini <santiagonob@gmail.com>
2020-05-04 22:51:06 -0300

    GEA Updated to Django 3 and all dependencies
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- estudio/settings.py
+++ estudio/settings.py
@@ -6,43 +6,39 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+g^%j#uc+n8-0_ski8w3o_8+-07l=24-4tt#n0a$k^co9!3f&x"
+SECRET_KEY = "d$476zo$0w1ab$@4*h!=ym_^0(g10n0gp6eu=kcr+&=p$e=-8!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 # Application definition
-INSTALLED_APPS = (
-    "grappelli",
-    "filebrowser",
+
+INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.sessions",
     "django.contrib.contenttypes",
+    "django.contrib.sessions",
     "django.contrib.messages",
-    # Disable Django's own staticfiles handling in favour of WhiteNoise, for
-    # greater consistency between gunicorn and `./manage.py runserver`. See:
-    # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
-    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
     "gea.apps.GeaConfig",
     "nested_admin",
-)
+    "django_extensions",
+]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
@@ -68,28 +64,43 @@ TEMPLATES = [
 WSGI_APPLICATION = "estudio.wsgi.application"
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": os.path.join(BASE_DIR, "gea.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gea",
+        "USER": "santi",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-dcapv = "django.contrib.auth.password_validation.%s"
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": dcapv % "UserAttributeSimilarityValidator",},
-    {"NAME": dcapv % "MinimumLengthValidator",},
-    {"NAME": dcapv % "CommonPasswordValidator",},
-    {"NAME": dcapv % "NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
-LANGUAGE_CODE = "es-AR"
-TIME_ZONE = "America/Argentina/Buenos_Aires"
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+LANGUAGE_CODE = "es-ar"
+TIME_ZONE = "America/Buenos_Aires"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -98,28 +109,17 @@ USE_TZ = True
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
-STATIC_URL = "/static/"
-# MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
-# MEDIA_URL = "/"
-
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "static"),
-]
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, "static")]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# Grappelli customization
-GRAPPELLI_ADMIN_TITLE = "GEA"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 # Redirection after login
 LOGIN_REDIRECT_URL = "home"
--- estudio/urls.py
+++ estudio/urls.py
@@ -1,19 +1,14 @@
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 from gea.views import Home
-from filebrowser.sites import site
-
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
-    url(r"^admin/filebrowser/", include(site.urls)),
-    url(r"^grappelli/", include("grappelli.urls")),  # grappelli URLS
-    url(r"^accounts/login/$", auth_views.login, name="login"),
-    url(r"^admin/", admin.site.urls),
-    url(r"^gea/", include("gea.urls")),
-    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
-    url(r"^$", Home.as_view(), name="home"),
-    url(r"^nested_admin/", include("nested_admin.urls")),
+    path("accounts/login/", auth_views.login, name="login"),
+    path("admin/", admin.site.urls),
+    path("gea/", include("gea.urls")),
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("", Home.as_view(), name="home"),
+    path("nested_admin/", include("nested_admin.urls")),
 ]
