51addde9e45592f7aa2cc05d3eda406d118fa70b
Santiago Pestarini <santiagonob@gmail.com>
2020-05-04 23:13:24 -0300

    fix login url
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- estudio/urls.py
+++ estudio/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
 
 from gea.views import Home
 
 urlpatterns = [
-    path("accounts/login/", auth_views.login, name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("admin/", admin.site.urls),
     path("gea/", include("gea.urls")),
     path("admin/doc/", include("django.contrib.admindocs.urls")),
