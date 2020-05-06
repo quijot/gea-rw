624507912f341f3e92566ea455794f37b64fa79d
Santiago Pestarini <santiagonob@gmail.com>
2020-05-06 18:05:47 -0300

    urls sanitized
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- estudio/urls.py
+++ estudio/urls.py
@@ -1,14 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
 from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
     path("admin/", admin.site.urls),
-    path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("gea/", include("gea.urls")),
     path("", RedirectView.as_view(url="gea/", permanent=True)),
     path("nested_admin/", include("nested_admin.urls")),
@@ -18,4 +15,4 @@ urlpatterns = [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Add Django site authentication urls (for login, logout, password management)
-# urlpatterns += [path("accounts/", include("django.contrib.auth.urls"))]
+urlpatterns += [path("accounts/", include("django.contrib.auth.urls"))]
