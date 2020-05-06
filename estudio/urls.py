00a188b2fe0f37e45e796c66473f31e1ff288a66
Santiago Pestarini <santiagonob@gmail.com>
2020-05-05 21:52:45 -0300

    update urls
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- estudio/urls.py
+++ estudio/urls.py
@@ -1,14 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.urls import include, path
-
-from gea.views import Home
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("admin/", admin.site.urls),
-    path("gea/", include("gea.urls")),
     path("admin/doc/", include("django.contrib.admindocs.urls")),
-    path("", Home.as_view(), name="home"),
+    path("gea/", include("gea.urls")),
+    path("", RedirectView.as_view(url="gea/", permanent=True)),
     path("nested_admin/", include("nested_admin.urls")),
 ]
+
+# Developing
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Add Django site authentication urls (for login, logout, password management)
+# urlpatterns += [path("accounts/", include("django.contrib.auth.urls"))]
